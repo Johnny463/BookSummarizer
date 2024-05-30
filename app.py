67dd2a77e297effa0ed41a56f4a135390823943d
Johnny463 <m.junaid72528@gmail.com>
2024-05-30 12:38:12 +0500

    Updated

--- app.py
+++ app.py
@@ -1,56 +1,47 @@
 from dotenv import load_dotenv
 import os
-from utils import clean_text
-from langchain.text_splitter import CharacterTextSplitter
+from utils import clean_text, create_pdf
 from PyPDF2 import PdfReader
 from langchain.docstore.document import Document
-from langchain.document_loaders.pdf import PDFMinerLoader
-from langchain_core.prompts import PromptTemplate
-from langchain_openai import ChatOpenAI
 from langchain.chains.summarize import load_summarize_chain
-import textwrap
-
+from langchain.llms import OpenAI
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import json
 load_dotenv()
+
 api_key = os.getenv("OPENAI_API_KEY")
+llm = OpenAI(temperature=0)
+def main():
+    summary_ = ""
+    pdf_reader = PdfReader('crime-and-punishment.pdf')
+    count=0
+    i=6 # starting chapter 1
+    j=23
+    while(count<49):
+        text = ""
+        #extracting text from 16 pages
+        for page in pdf_reader.pages[i:j]:
+                text += page.extract_text()
+                
+        #cleans text
+        text=clean_text(text)         
+        text_splitter = RecursiveCharacterTextSplitter(
+                    chunk_size=1000,
+                    chunk_overlap=0,
+                    length_function=len
+                    )
+        chunks = text_splitter.split_text(text=text)
+        docs = [Document(page_content=t) for t in chunks]
+        chain = load_summarize_chain(llm, chain_type="map_reduce")
+        summary = chain.run(docs)
+        summary_=summary_+summary
+        create_pdf(summary_)
+        i=i+16
+        j=j+16
+        count=count+1
+        if os.path.exists("summary.json"):
+                with open("summary.json", "w") as f:
+                      json.dump(summary_, f, indent=4)
+if __name__ == "__main__":
+      main()
 
-text_splitter = CharacterTextSplitter(separator="chapter", chunk_overlap=100, chunk_size=15000, is_separator_regex=False, keep_separator=True)
-pdf_reader = PDFMinerLoader('crime-and-punishment.pdf')
-docs = pdf_reader.load_and_split(text_splitter)
-
-cleaned_docs = []
-for ind, doc in enumerate(docs):
-    cleaned_docs.append(Document(page_content=clean_text(doc.page_content), metadata=doc.metadata))
-
-# Split the last chapter into two sections: chapter and epilogue
-last_doc = cleaned_docs.pop()  # Remove the last document from the list
-last_chapter, epilogue = last_doc.page_content.split('Epilogue')
-cleaned_docs.append(Document(page_content=last_chapter, metadata=last_doc.metadata))
-cleaned_docs.append(Document(page_content=f'Epilogue{epilogue}', metadata=last_doc.metadata))
-
-# Define prompts for chapter summary and final summary
-map_prompt = """Generate a summary of this chapter of book 'Crime and Punishment' by Fyodor Dostoevsky. It should be a 1 page summary so approximately 300 words. Do not miss important details.
-
-{text}
-
-CHAPTER SUMMARY:"""
-MAP_PROMPT = PromptTemplate(template=map_prompt, input_variables=["text"])
-
-combine_prompt = """Combine the below summaries of chapters of book 'Crime and Punishment' by Fyodor Dostoevsky to create a final summary of the book. It should be contain 20 pages so approximately 5500 words.
-
-{text}
-
-20 PAGE SUMMARY:"""
-COMBINE_PROMPT = PromptTemplate(template=combine_prompt, input_variables=["text"])
-
-# Initialize the OpenAI model
-model = "gpt-3.5-turbo-1106"  # 16k context window
-llm = ChatOpenAI(api_key=api_key, temperature=0, model_name=model, max_retries=5)
-
-# Load the summarization chain
-chain = load_summarize_chain(llm, chain_type="map_reduce", combine_prompt=COMBINE_PROMPT, map_prompt=MAP_PROMPT)
-
-# Run the summarization chain on cleaned documents
-result = chain.run(cleaned_docs)
-
-# Print the final summary
-print('\n'.join(textwrap.wrap(result, 80)))
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import re
-
+from fpdf import FPDF
+   
 
 def clean_text(text):
     # if new line if followed by - join the lines
@@ -25,3 +26,39 @@ def clean_text(text):
     # remove double or more spaces
     text = re.sub(r'\s+', ' ', text)
     return text.strip()
+
+def create_pdf(summary):
+    
+
+    # Define the PDF document
+    class PDF(FPDF):
+        def footer(self):
+            self.set_y(-15)
+            self.set_x(-5)
+            self.set_font('Arial', 'I', 8)
+            self.cell(0, 10, 'Page ' + str(self.page_no()), 0, 0, 'C')
+
+    # Create instance of FPDF class
+    pdf = PDF()
+    pdf.set_left_margin(22)
+    pdf.set_right_margin(22)
+    pdf.add_page()
+
+    # add title and author in the center of first page
+    pdf.set_font('Arial', 'B', 16)
+    pdf.cell(0, 10, 'Summary of Crime and Punishment', 0, 1, 'C')
+    pdf.set_font('Arial', 'I', 12)
+    pdf.cell(0, 1, 'by Fyodor Dostoevsky', 0, 1, 'C')
+
+    # add space between title and content
+    pdf.ln(8)
+
+    # Add content
+    pdf.set_font('Arial', '', 13)
+    pdf.multi_cell(0, 10, summary)
+
+    # Saving the PDF file
+    pdf_output_path = 'Crime_and_Punishment_Summary.pdf'
+    pdf.output(pdf_output_path)
+
+    pdf_output_path
